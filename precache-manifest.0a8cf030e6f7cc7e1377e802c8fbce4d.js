e08c88ffda50843a543af986f390f6c45f0e72ba
Valentin Hervieu <valentin@hervi.eu>
2019-08-11 23:07:11 +0200

    Updates

---
+++ precache-manifest.0a8cf030e6f7cc7e1377e802c8fbce4d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da32d2eadd27f985b593abd30fe6580e",
+    "url": "/bundlelove/index.html"
+  },
+  {
+    "revision": "c1e2e3f171efff5e3079",
+    "url": "/bundlelove/static/js/2.ecc7ef71.chunk.js"
+  },
+  {
+    "revision": "398c3f7672065a680f57",
+    "url": "/bundlelove/static/js/main.758eb32e.chunk.js"
+  },
+  {
+    "revision": "f6d66a042ecc81f412da",
+    "url": "/bundlelove/static/js/runtime~main.8af550e1.js"
+  }
+]);
